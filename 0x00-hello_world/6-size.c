f6ec9f3b25b8247ebd8949aab245e3e5f854e9b2
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-16 11:35:53 -0700

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a c program to prints the sizes of datatype of a system
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %c byte(s)\n", size of a);
+	printf("size of an int: %d byte(s)\n", size of b);
+	printf("size of a long int: %ld bytes(s)\n", size of c);
+	printf("size of a long long int: %lld bytes(s)\n", size of d);
+	printf("size of a float: %f bytes(s)\n", size of f);
+	return (0);
+}
