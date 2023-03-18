b1cd40031c351c0d641c349f6c3fd5d72cc4f7ab
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-18 09:28:07 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print number 0 to 9
+ *
+ * Return: Always 0 (correct)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
