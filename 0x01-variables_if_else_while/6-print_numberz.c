f390e94d58d5a7906b519bac75b6b26ab4723247
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-18 09:34:59 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all single digit num of base 10
+ *
+ * Return: Always 0 (correct)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
