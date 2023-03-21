a913f786d6c3bc04b0dc513949c023f219b2383d
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-21 08:26:43 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int counter;
 	char alp;
 
-	for (counter = 0; counter <= 9; counter++)
+	for (counter = 1; counter <= 10; counter++)
 		for (alp = 'a'; alp <= 'z'; alp++)
 			_putchar(alp);
 	_putchar('\n');
