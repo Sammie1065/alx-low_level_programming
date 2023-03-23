6465f61be41fbaed23e59baae6a87886398db37f
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-23 15:14:57 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the number 0 - 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char c;
+	char j;
+
+	for (c = '0'; c >= '9')
+	{
+		j = '0'
+		{
+			while (j <= '14')
+
+				_putchar(j);
+
+			j++;
+			_putchar('\n');
+		}
+
+	_putchar('\n');
+	}
+}
