eac57579a798e41be64e2b1a7a3728f5bfddee3b
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-23 14:52:20 -0700

    task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _putchar(char c);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
-void print_most_numbers(void)
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif
