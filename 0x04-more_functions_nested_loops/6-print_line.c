fe7a602374ee03c05f320ccad3992823652a84d7
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-23 15:45:33 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - to print a new line
+ * @n: integer to he used
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	for (n = 1; n <= 14;)
+		_putchar('_');
+
+		_putchar('\n');
+}
