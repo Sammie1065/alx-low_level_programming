11f36ad31792de72f9a9643e80c389b94d5f0920
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-23 08:52:22 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - to print numbers from 0 to 9
+ * @i: number to be used
+ */
+
+void print_numbers(int i)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+
+		_putchar('i');
+	_putchar('\n');
+}
