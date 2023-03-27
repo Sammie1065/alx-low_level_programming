f3900885baeb012f49e92c7ce7b9ea6bd6916857
Sammie1065 <Okonsamuel545@gmail.com>
2023-03-27 08:56:22 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - a program to swap two integers
+ * @a: int to be used
+ * @b: int to be used
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *b;
+}
