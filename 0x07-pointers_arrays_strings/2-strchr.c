9c61621b391f3e1c2b466ff7bc2ae2ad14a7ba6b
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-04 13:02:51 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,17 +4,17 @@
  * *_strchr - a func. that locates a char. in a string
  * @c: occurence of the char.
  * @s: in the string
- * Return: a pointer to the first occurence of a char
+ * Return: a pointer to the first occurence of a char else NULL
  */
 
 char *_strchr(char *s, char c)
 {
 	while (*s)
 	{
-		if (*S != c)
+		if (*s != c)
 			s++;
 		else
-			return (S);
+			return (s);
 	}
 
 	if (c == '\0')
