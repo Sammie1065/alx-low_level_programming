6d136f835eeb62c5236933e832720db1e38faf5c
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-05 13:30:55 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a func. that prints a string
+ * @s: string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchsr(*s)
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
