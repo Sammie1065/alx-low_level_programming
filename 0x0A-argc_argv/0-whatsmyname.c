24915bc0293aa0ec6817235c819f8ee122191e34
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-09 09:53:26 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a prog. that prints its name followed by a new line
+ * @argc: a command line argument count
+ * @argv: command line argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("program name is : %s\n", argv[0]);
+
+	return (0);
+}
