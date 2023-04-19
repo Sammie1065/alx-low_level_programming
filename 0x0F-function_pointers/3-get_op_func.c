71c3c501f06e177161f524d8e145e5ba02354949
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-19 15:44:15 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,5 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * get_op_func - a func. that selects the correct function
@@ -22,15 +21,9 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL },
 	};
 
-	int i = 0;
+	int j = 0;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op == 0)
-				return (ops[i].f);
-
-				i++;
-	}
-
-	return (0);
+	while (ops[j].op != NULL && *(ops[j].op) != *s)
+		j++;
+	return (ops[j].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * main - main func.
