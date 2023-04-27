b9ec2ff8265c477171204a5694cd59d88426ef17
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-27 12:03:33 -0700

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+	extern  printf		; the c function, to be called
+
+		sectin .data		; Data section, initialized variables
+ msg:		db "Hello, Holberton", 0 ; C strings needs 0
+ fmt:		sb "%s", 10, 0		; The printf format, "\n",'0'
+
+		section .text		; code section.
+
+		global main		; the standard gcc entry point
+ main:					; the program label for the entry point
+		push	rbp		; set up stack frame, must be alligned
+
+		mov	rdi,fmt
+		mov	rsi,msg
+		mov	rax,0		; or can be xor  rax,rax
+		call	printf		; Call C func.
+
+		pop	rbp		; restore stack
+
+		mov	rax,0		; normal, no error, return value
+		ret			; return
