d2a359992e6b1c34087d652130948ab84eb8460b
Sammie1065 <Okonsamuel545@gmail.com>
2023-04-27 08:07:49 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 /**
- * print_string - a func that a string before the main func is executed
+ * myBringupFun - a func that prints out something nice before
+ * the main func is executed
  */
 
-void print_string(void)
+void myBringupFun(void) __attribute__ ((constructor));
+
+
+void myBringupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - entry point
- * Return: 0
- */
-
-int main(void)
-{
-	char *output;
-
-	output = print_string();
-
-	return (0);
 }
