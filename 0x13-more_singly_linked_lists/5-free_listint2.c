bca89c104d28b80434ec21a26936a19217e81956
Sammie1065 <Okonsamuel545@gmail.com>
2023-05-01 15:43:04 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a func that frees a listint_t list.
+ * @head: a double pointer of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return (NULL);
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
