74476a50714ce088e97539f00c0b5b43cd5ef839
Sammie1065 <Okonsamuel545@gmail.com>
2023-05-02 11:19:39 -0700

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * _ra - a func that reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: this is the old list to be attached
+ * @size: this is the size of the new list
+ * @new:  the new node to be added to the list
+ * Return: a pointer to the new list
+ */
+
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t j;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (j = 0; j < size - 1; j++)
+		newlist[j] = list[j];
+	newlist[j] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * free_listint_safe - a func that frees a listint_t list.
+ * @head: a double pointer to the start of the list
+ *
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **head)
+{
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (I = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
+}
