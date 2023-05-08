53395a9f924ca2b39bc14e2ced9a91f0d9fabeaa
Sammie1065 <Okonsamuel545@gmail.com>
2023-05-08 09:57:13 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	ssize_t o, r, W;
 	char *buffer;
 
 	if (filename == NULL)
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (o == -1 || r == -1 || W == -1 || W != r)
 	{
 		free(buffer);
 		return (0);
@@ -38,5 +38,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	close(o);
 
-	return (w);
+	return (W);
 }
