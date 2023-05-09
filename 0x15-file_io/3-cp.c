1fa92fc2627ff770834e49c01eb7aa3a9c8a3432
Sammie1065 <Okonsamuel545@gmail.com>
2023-05-08 17:22:15 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ void check97(int argc)
 {
 	if (argc != 3)
 	{
-		dprint(STDERR_FILENO, "usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
 		exit(97);
 	}
 }
@@ -32,7 +32,7 @@ void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		dprint(STDERR_FILENO, "Error: can't read from file %s\n", file);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", file);
 		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
@@ -55,7 +55,7 @@ void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		dprint(STDERR_FILENO, "Error: can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file);
 		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
@@ -75,7 +75,7 @@ void check100(int check, int fd)
 {
 	if (check == -1)
 	{
-		dprint(STDERR_FILENO, "Error: can't close fd%d\n", fd);
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -98,7 +98,8 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fd_to = open(argv[2], fd_from, -1);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
 	{
